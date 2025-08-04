e11f3ac3090494abdfa293483f32873c6b3ba3e3
ianmabruk <ianmabruk3@gmail.com>
2025-08-04 16:09:49 +0300

    mt-type

--- data_type.py
+++ data_type.py
@@ -1,17 +1,17 @@
 #snake_case
 
 x=10#integer
-print(x)
+print(x,"hello",type(x))
 pi=3.142#floating
-print(pi)
+print(type(pi))
 my_Name="ian"#string
-print(my_Name)
+print(type(my_Name))
 my_Name='ian'#string
-print(my_Name)
+print(type(my_Name))
 is_happy=True#boolean
 print(is_happy)
 no_value=None#null/none is the equivalent
-print(no_value)
+print(type(no_value))
 
 ##
 
@@ -21,10 +21,10 @@ print(no_value)
 
   #Advanced Datatypes
 my_list=[10,10,"string,True,None,123,[]"] #list/<Array>can repeat numbers my_list[o]
-print(my_list)
+print(type(my_list[0]))
 my_set={1,2,3}#Set and Array ,,cannot repeat numbbers 
-print(my_set)
+print(type(my_set))
 my_tuple=(1,3,4,5,"Apple")#tupple  to access it use-my _tuple[4]=Apple
-print(my_tuple)
+print(type(my_tuple))
 my_object={"key":"value","age":23}  #we call a Dictionary Key-value
-print(my_object)
+print(type(my_object))
