183d5848c2f7577fd172dce23648d0442aa98151
ianmabruk <ianmabruk3@gmail.com>
2025-08-04 15:57:04 +0300

    datatype

---
+++ data_type.py
@@ -0,0 +1,30 @@
+#snake_case
+
+x=10#integer
+print(x)
+pi=3.142#floating
+print(pi)
+my_Name="ian"#string
+print(my_Name)
+my_Name='ian'#string
+print(my_Name)
+is_happy=True#boolean
+print(is_happy)
+no_value=None#null/none is the equivalent
+print(no_value)
+
+##
+
+#def myfun():
+  #  m=23
+
+
+  #Advanced Datatypes
+my_list=[10,10,"string,True,None,123,[]"] #list/<Array>can repeat numbers my_list[o]
+print(my_list)
+my_set={1,2,3}#Set and Array ,,cannot repeat numbbers 
+print(my_set)
+my_tuple=(1,3,4,5,"Apple")#tupple  to access it use-my _tuple[4]=Apple
+print(my_tuple)
+my_object={"key":"value","age":23}  #we call a Dictionary Key-value
+print(my_object)
